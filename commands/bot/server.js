4907f3e517a0f97556a62e08a60d8d347f881385
4wip <168364544+4wip@users.noreply.github.com>
2024-05-07 15:06:49 +0200

    Update server.js

--- commands/bot/server.js
+++ commands/bot/server.js
@@ -47,7 +47,7 @@ module.exports = {
 
 					let guild = null;
 
-					if (!args[1]) return message.channel.send("Merci d'entrer le nom d'un serveur ou je me trouve")
+					if (!args[1]) return message.channel.send("Merci d'entrer le nom d'un serveur où je me trouve")
 
 					if (args[1]) {
 						let fetched = client.guilds.cache.find(g => g.name.toLowerCase().includes(args.slice(1).join(" ")));

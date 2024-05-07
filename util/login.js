30e1d0cc3a4b834c69be3043b56e3791abedfecc
4wip <168364544+4wip@users.noreply.github.com>
2024-05-07 11:00:32 +0200

    Update login.js

--- util/login.js
+++ util/login.js
@@ -1,36 +1,29 @@
 const {
-	readdirSync
+    readdirSync
 } = require('fs');
 
-
-
 const login = (client) => {
-	const Discord = require("discord.js")
-	const logs = require('discord-logs');
-	logs(client)
-	const disbut = require('discord-buttons');
-	disbut(client);
-	const tempo = require("./gestion/tempo.js");
-	tempo(client)
+    const Discord = require("discord.js")
+    const logs = require('discord-logs');
+    logs(client)
+    const disbut = require('discord-buttons');
+    disbut(client);
+    const tempo = require("./gestion/tempo.js");
+    tempo(client)
 
-	client.config = require("../config.json")
-	client.cooldown = new Array();
-	client.interaction = {}
-	client.guildInvites = new Map();
-	client.queue = new Map();
-	client.commands = new Discord.Collection()
-	client.aliases = new Discord.Collection()
-	client.snipes = new Map()
-	client.inter = new Array()
+    client.config = require("../config.json")
+    client.cooldown = new Array();
+    client.interaction = {}
+    client.guildInvites = new Map();
+    client.queue = new Map();
+    client.commands = new Discord.Collection()
+    client.aliases = new Discord.Collection()
+    client.snipes = new Map()
+    client.inter = new Array()
 
-
-	client.login(process.env.token);
-
+    client.login(client.config.token);
 }
 
-
-
 module.exports = {
-
-	login
+    login
 }

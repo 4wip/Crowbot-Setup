99be8a54e8da2d7b71657f6b0a5dd18a4609c184
4wip <168364544+4wip@users.noreply.github.com>
2024-05-07 14:53:30 +0200

    Update serverinfo.js

--- commands/utilitaire/serverinfo.js
+++ commands/utilitaire/serverinfo.js
@@ -30,7 +30,7 @@ module.exports = {
 				NONE: '0',
 				LOW: 'Faible',
 				MEDIUM: 'Moyen',
-				HIGH: 'Elevé',
+				HIGH: 'Élevé',
 				VERY_HIGH: 'Très élevé',
 			};
 
@@ -41,19 +41,19 @@ module.exports = {
 			//console.log(NoRoles)
 			const ServerInfo = new Discord.MessageEmbed()
 				.setTitle(`${guild.name} `)
-				.setURL("https://discord.gg/9ZfB8m5E88")
-				.addField(`Identifiant Serveur <a:serial_warning:1220471714056507535> :`, `__${guild.id}__`, true)
+				.setURL("https://discord.gg/PuqkTuxtFA")
+				.addField(`Identifiant Serveur <a:4_warning:1220471714056507535> :`, `__${guild.id}__`, true)
 				.addField(`Niveau De Vérification :`, `__${verificationLevels[message.guild.verificationLevel]}__`, true)
-				.addField(`Nombre De Membre(s) <:serial_membre:1217042139037438032> :`, `__${guild.memberCount}__`, true)
-				.addField(`Nombre De Membre(s) Actif(s) <a:serial_terre:1220445710223151217> :`, `__${guild.members.cache.filter(m => m.presence?.status === 'online' || m.presence?.status === 'dnd' || m.presence?.status === 'streaming' || m.presence?.status === 'idle').size}__`, true)
-				.addField(`Nombre D'humain(s) <:serial_membre:1220474644004339713> :`, `__${guild.members.cache.filter((m) => !m.user.bot).size}__`, true)
+				.addField(`Nombre De Membre(s) <:4_membre:1217042139037438032> :`, `__${guild.memberCount}__`, true)
+				.addField(`Nombre De Membre(s) Actif(s) <a:4_terre:1220445710223151217> :`, `__${guild.members.cache.filter(m => m.presence?.status === 'online' || m.presence?.status === 'dnd' || m.presence?.status === 'streaming' || m.presence?.status === 'idle').size}__`, true)
+				.addField(`Nombre D'humain(s) <:4_membre:1220474644004339713> :`, `__${guild.members.cache.filter((m) => !m.user.bot).size}__`, true)
 				.addField(`Nombre De Bot(s) :robot: :`, `__${guild.members.cache.filter((m) => m.user.bot).size}__`, true)
-				.addField(`Nombre D'utilisateur(s) En Vocal <:serial_vocal:1217007411068669963> :`, `__${guild.members.cache.filter(m => m.voice.channel).size}__`, true)
+				.addField(`Nombre D'utilisateur(s) En Vocal <:4_vocal:1217007411068669963> :`, `__${guild.members.cache.filter(m => m.voice.channel).size}__`, true)
 				.addField(`Nombre D'utilisateur(s) Sans Rôle :x: :`, `__${NoRoles}__`, true)
-				.addField(`Nombre De Boost(s) <a:serial_boost:1217008346662699098> :`, `__${guild.premiumSubscriptionCount}__`, true)
-				//.addField(`Nombre De Booster(s) <a:serial_nitroboost:1217195480052666378> :`, `${boosters}`, true)
+				.addField(`Nombre De Boost(s) <a:4_boost:1217008346662699098> :`, `__${guild.premiumSubscriptionCount}__`, true)
+				//.addField(`Nombre De Booster(s) <a:4_nitroboost:1217195480052666378> :`, `${boosters}`, true)
 				.addField(`Nombre De Rôle(s) :scroll: :`, `__${guild.roles.cache.size}__`, true)
-				.addField(`Nombre De Salon(s) <a:serial_chat:1220445576467058739> :`, `__${guild.channels.cache.size}__`, true)
+				.addField(`Nombre De Salon(s) <a:4_chat:1220445576467058739> :`, `__${guild.channels.cache.size}__`, true)
 				.addField(`Nombre D'émoji(s) Total :package: :`, `__${guild.emojis.cache.size}__`, true)
 				.setImage(message.guild.bannerURL({size: 1024}))
 				.setFooter("La Création Du Serveur Remonte au :")

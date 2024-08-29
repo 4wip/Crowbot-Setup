7c4b82a766dd4568a3f8a74a7a641b364c487c0c
XIV <168364544+4wip@users.noreply.github.com>
2024-08-29 12:23:20 +0200

    Update voice.js

--- commands/utilitaire/voice.js
+++ commands/utilitaire/voice.js
@@ -1,125 +1,37 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
-const {
-    MessageActionRow,
-    MessageButton,
-    MessageMenuOption,
-    MessageMenu
-} = require('discord-buttons');
+const Discord = require('discord.js');
+const db = require('quick.db');
 
 module.exports = {
     name: 'voice',
     aliases: ['vc', 'stats', 'stat'],
 
     run: async (client, message, args, prefix, color) => {
-        let perm = ""
+  let perm = false;
         message.member.roles.cache.forEach(role => {
-            if (db.get(`modsp_${message.guild.id}_${role.id}`)) perm = true
-            if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = true
-            if (db.get(`ownerp_${message.guild.id}_${role.id}`)) perm = true
-        })
-            if(client.config.owner.includes(message.author.id) || db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || perm) {
-                const guild = client.guilds.cache.get(args[0]) || message.guild
+            if (db.get(`ownerp_${message.guild.id}_${role.id}`)) perm = true;
+            if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = true;
+        });
 
-        if (args[0] === "all") {
+        if (client.config.owner.includes(message.author.id) || db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || perm) {
+            const members = await message.guild.members.fetch();
+            const onlineMembers = members.filter(member => member.presence.status !== 'offline');
+            const voiceMembers = members.filter(member => member.voice.channel);
+            const boosts = message.guild.premiumSubscriptionCount;
+            const streamingMembers = voiceMembers.filter(member => member.voice.streaming);
 
-            var streamingCount = 0;
-            var mutedCount = 0;
-            var mutedMic = 0;
-            var cameraCount = 0;
-            var connectedCount = 0;
-
-            const channels = message.guild.channels.cache.filter(c => c.type === 'voice');
-            channels.forEach(c => {
-                connectedCount += c.members.size;
-                c.members.forEach(m => {
-                    if (m.voice.streaming) streamingCount++;
-                    if (m.voice.selfDeaf || m.voice.serverDeaf) mutedCount++;
-                    if (m.voice.selfMute || m.voice.serverMute) mutedMic++;
-                    if (m.voice.selfVideo) cameraCount++;
-                })
-            })
-            const voiceConnectedEmbed = new Discord.MessageEmbed()
-                .setTitle(`__${message.guild.name} ➔ Statistiques__`)
-                .setURL('https://github.com/4wip')
-                //.setThumbnail(guild.iconURL({dynamic: true}))
-                .setDescription(`
-- ${message.guild.memberCount > 1 ? '*Membres*' : '*Membre*'} *sur le serveur :* **${message.guild.memberCount}** 
-- ${message.guild.members.cache.filter(m => m.user.presence.status !== 'offline').size > 1 ? '*Membres*' : '*Membre*'} *en ligne :* **${message.guild.members.cache.filter(m => m.user.presence.status !== 'offline').size}** 
-- ${message.guild.members.cache.filter(m => m.voice.channel).size  > 1 ? '*Membres*' : '*Membre*'} *en vocal :* **${message.guild.members.cache.filter(m => m.voice.channel).size}** 
-- ${message.guild.premiumSubscriptionCount > 1 ? '*Nombre*' : '*Nombres*'} *de boosts :* **${message.guild.premiumSubscriptionCount}** 
-`)
+            const embed = new Discord.MessageEmbed()
                 .setColor(color)
-                .setTimestamp()
-                .setFooter(`${message.guild.name} #Statistiques`)
+                .setThumbnail(message.guild.iconURL())
+                .setTitle(`${message.guild.name} Statistiques!`)
+                .setDescription(
+                    `*Membres:* **${members.size}**\n` +
+                    `*En ligne:* **${onlineMembers.size}**\n` +
+                    `*En vocal:* **${voiceMembers.size}**\n` +
+                    `*En stream:* **${streamingMembers.size}**\n` +
+                    `*Boosts:* **${boosts}**`
+                );
 
-                if (guild.icon) voiceConnectedEmbed.setThumbnail(guild.iconURL({
-                    dynamic: true
-                }))
-
-            return message.channel.send(voiceConnectedEmbed)
-        } else if (!args[1]) {
-            let embed = new Discord.MessageEmbed()
-                .setTimestamp()
-				.setTitle(`__${message.guild.name} ➔ Statistiques Salon Vocaux__`)
-				.setURL('https://github.com/4wip')
-				.setThumbnail(guild.iconURL({dynamic: true}))
-                .setDescription(`- *Il y à actuellement* **${message.guild.members.cache.filter(m => m.voice.channel).size} ${message.guild.members.cache.filter(m => m.voice.channel).size  > 1 ? 'Personnes' : 'Personne'}** *en vocal sur le serveur.* <:4_vocal:1217007411068669963>`)
-                .setColor(color)
-                .setFooter(`${message.guild.name} #Statistiques`)
-
-            message.channel.send(embed)
-
-        } else if (!args[0] || args[0] === "info") {
-            if (client.config.owner.includes(message.author.id) || db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || perm) {
-                if (args[1] === "all") {
-
-                    var streamingCount = 0;
-                    var mutedCount = 0;
-                    var mutedMic = 0;
-                    var cameraCount = 0;
-                    var connectedCount = 0;
-
-                    const channels = message.guild.channels.cache.filter(c => c.type === 'voice');
-                    channels.forEach(c => {
-                        connectedCount += c.members.size;
-                        c.members.forEach(m => {
-                            if (m.voice.streaming) streamingCount++;
-                            if (m.voice.selfDeaf || m.voice.serverDeaf) mutedCount++;
-                            if (m.voice.selfMute || m.voice.serverMute) mutedMic++;
-                            if (m.voice.selfVideo) cameraCount++;
-                        })
-                    })
-                    const voiceConnectedEmbed = new Discord.MessageEmbed()
-                        .setTitle(`__${message.guild.name} ➔ Statistiques__`)
-                        .setURL('https://github.com/4wip')
-                        .setThumbnail(guild.iconURL({dynamic: true}))
-                        .setDescription(` 
-- **${message.guild.members.cache.filter(m => m.voice.channel).size}** ${message.guild.members.cache.filter(m => m.voice.channel).size  > 1 ? '*Personnes*' : '*Personne*'} *en vocal.* <:4_vocal:1217007411068669963>
-- **${mutedMic}** ${mutedMic > 1 ? '*Personnes*' : '*Personne*'} *sont mute micro.* 
-- **${mutedCount}** ${mutedCount > 1 ? '*Personnes*' : '*Personne*'} *sont mute casque.* 
-- **${streamingCount}** ${streamingCount > 1 ? '*Personnes*' : '*Personne*'} *sont en stream.* 
-- **${cameraCount}** ${cameraCount > 1 ? '*Personnes*' : '*Personne*'} *sont en caméra.* 
-`)
-                        .setColor(color)
-                        .setTimestamp()
-                        .setFooter(`${message.guild.name} #Statistiques`)
-
-                    return message.channel.send(voiceConnectedEmbed)
-                } else if (!args[1]) {
-                    let embed = new Discord.MessageEmbed()
-                        .setTimestamp()
-                        .setTitle(`__${message.guild.name} ➔ Statistiques__`)
-                        .setURL('https://github.com/4wip')
-						.setThumbnail(guild.iconURL({dynamic: true}))
-                        .setDescription(`- *Il y à actuellement* **${message.guild.members.cache.filter(m => m.voice.channel).size} ${message.guild.members.cache.filter(m => m.voice.channel).size  > 1 ? 'Personnes' : 'Personne'}** *en vocal sur le serveur.* <:4_vocal:1217007411068669963>`)
-                        .setColor(color)
-                        .setFooter(`${message.guild.name} #Statistiques`)
-
-                    message.channel.send(embed)
-                }
-            }
+            message.channel.send(embed);
         }
     }
-}
-    }
+};
